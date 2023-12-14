071443ddd94901cc610773b608063376cac3d50a
nautilus@octopus <nautilus@trovaprezzi.it>
2023-12-14 16:37:28 +0100

    frequency

--- tools/char-frequency.js
+++ tools/char-frequency.js
@@ -2,32 +2,50 @@
 
 class CharsFrequency {
     constructor() {
-        this._histogram = new Map();
+        this.histogram = new Map();
     }
 
     add(char) {
-        if (this._histogram.has(char)) {
-            let current = this._histogram.get(char)
-            this._histogram.set(char, current + 1)
+        if (this.histogram.has(char)) {
+            let current = this.histogram.get(char)
+            this.histogram.set(char, current + 1)
         } else {
-            this._histogram.set(char, 1)
+            this.histogram.set(char, 1)
         }
     }
 
-    get histogram() {
-        return this._histogram;
+    total_count() {
+        return [...this.histogram.values()].reduce((total, current) => (total + current));
     }
 
+    chars_histogram() {
+        let chars_histogram = new Map();
+        let total = this.total_count();
+
+        for (let key of this.histogram.keys()) {
+            let percentage = ((this.histogram.get(key) / total) * 100).toFixed(2);
+            chars_histogram.set(key, percentage);
+        }
+
+        return chars_histogram;
+    }
 }
 
 chars_frequency = new CharsFrequency();
 
 process.stdin.setEncoding("utf-8");
 process.stdin.on('data', function (text) {
-    text = text.replace(/\s/g, '').toLowerCase();
+    text = text.match(/[a-zA-Z0-9]+/g).join('').toUpperCase();
     for (let char of text) {
         chars_frequency.add(char)
     }
 });
 
-console.log(chars_frequency.histogram);
+process.stdin.on('end', function () {
+    let chars_histogram_array = [...chars_frequency.chars_histogram()];
+    chars_histogram_array.sort((a, b) => b[1] - a[1]);
+
+    for (let char of chars_histogram_array) {
+        console.log(`${char[0]}: ${'#'.repeat(char[1])} ${char[1]}%`);
+    }
+});
