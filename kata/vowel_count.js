6d89869d04ef5578b80c34f1135ebe2480b69344
nautilus@octopus <nautilus@trovaprezzi.it>
2023-12-20 14:54:28 +0100

    kata

---
+++ kata/vowel_count.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function getCount(str) {
+    let result = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+
+    let text = str.toLowerCase();
+    for (let i = 0; i < text.length; i++) {
+        if (vowels.includes(text[i])) {
+            result++;
+        }
+    }
+    return result;
+}
+
+function getCount_2(str) {
+    let result = 0, vowels = ['a', 'e', 'i', 'o', 'u']
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char))
+            result++;
+    }
+    return result;
+}
