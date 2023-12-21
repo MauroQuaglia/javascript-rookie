045bfaad6c651eaf16fee94bbfd20fc9b30d8453
nautilus@octopus <nautilus@trovaprezzi.it>
2023-12-21 15:49:56 +0100

    fix

--- kata/count_characters_in_your_string.js
+++ kata/count_characters_in_your_string.js
@@ -10,13 +10,3 @@ function count(string) {
     }
     return result;
 }
-
-
-/*
-test('', {});
-test('a', {'a': 1});
-test('ab', {'a': 1, 'b': 1});
-test('aba', {'a': 2, 'b': 1});
-test('ABC', {'A': 1, 'B': 1, 'C': 1});
-
-*/
