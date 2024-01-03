b562d4fd8ffb43107b5a3ead80783694155bef44
nautilus@octopus <nautilus@trovaprezzi.it>
2024-01-03 08:48:37 +0100

    Fix kata.

--- kata/buddy_pair.js
+++ kata/buddy_pair.js
@@ -6,17 +6,19 @@ function buddy(start, limit) {
     for (let n = start; n <= limit; n++) {
         let sum_n = divisors_sum(n);
         let m = sum_n - 1;
-        if (m > n && divisors_sum(m) === (n + 1)) {
-            return [n, m];
-        }
+        if (m > n && divisors_sum(m) === (n + 1)) return [n, m];
     }
     return "Nothing";
 }
 
 function divisors_sum(number) {
     let total = 1;
-    for (let i = 2; i <= number / 2; i++) {
-        if (number % i === 0) total += i;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number%i === 0) {
+            total += i;
+            let j = number/i;
+            if (j !== i) total += j;
+        }
     }
     return total;
 }
--- kata/tiny_function.js
+++ kata/tiny_function.js
@@ -1,18 +1,11 @@
 // https://www.codewars.com/kata/65552e9756ea20197078a163
 
-'use strict';
+//'use strict';
 
-// 't'.charCodeAt(0)
-// 'i'.charCodeAt(0)
-// 'n'.charCodeAt(0)
-// 'y'.charCodeAt(0)
+f=(a=''+1/0)=>a[6]+a[5]+a[4]+a[7]
 
-function f() {
-    return String.fromCharCode(116, 105, 110, 121);
-}
+// function f() {
+//     return String.fromCharCode(116, 105, 110, 121);
+// }
 
-// f=()=>'\x74\x69\x6e\x79';
-
-// f=()=>String.fromCharCode(116,105,110,121);
-
-f=()=>['','74','69','6e','79'].join('\\x')
+//f=()=>['','74','69','6e','79'].join('\\x')
