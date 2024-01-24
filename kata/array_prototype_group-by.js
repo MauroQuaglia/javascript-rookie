6e7394ff50dd756a201eb8195fdded8d5a569dca
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2024-01-24 14:29:38 +0100

    Fix

--- kata/array_prototype_group-by.js
+++ kata/array_prototype_group-by.js
@@ -2,51 +2,20 @@
 
 'use strict';
 
-Array.prototype.groupBy = function () {
-    let result = new Map();
-    for (let element of this) {
-        if (result.has(element)) {
-            result.get(element).push(element);
-        } else {
-            result.set(element, [element])
-        }
-    }
-    return result;
-}
-
-
 Array.prototype.groupBy = function (fn) {
     if (!fn) {
         fn = (val) => val;
     }
 
-    let result = new Map();
+    let result = {};
     for (let element of this) {
-        let key = fn(element);
-        if (result.has(key)) {
-            result.get(key).push(element);
+        let p = fn(element);
+        if (result[p]) {
+            result[p].push(element);
         } else {
-            result.set(key, [element])
+            result[p] = [element];
         }
     }
 
     return result;
 }
-
-/*
-    [1,2,3,2,4,1,5,1,6].groupBy()
-{
-    1: [1, 1, 1],
-    2: [2, 2],
-    3: [3],
-    4: [4],
-    5: [5],
-    6: [6]
-}
-
-[1,2,3,2,4,1,5,1,6].groupBy(function(val) { return val % 3;} )
-{
-    0: [3, 6],
-    1: [1, 4, 1, 1],
-    2: [2, 2, 5]
-}*/
