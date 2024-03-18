0e0e75ba5ad4b230f90a1044e4c42f86f824f474
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2024-03-18 17:39:50 +0100

    test

--- promises/promise_sample.js
+++ promises/promise_sample.js
@@ -1,3 +1,21 @@
+/* SINTASSI
+
+let promise = new Promise(function (resolve, reject) {
+    // resolve(value) oppure resolve(nuova-promise)
+    // reject(new Error())
+});
+OPPURE
+let promise = new Promise((resolve, reject) => {
+    // resolve(value) oppure resolve(nuova-promise)
+    // reject(new Error())
+});
+
+promise.then(valore_precedente => mia_callback(valore_precedente))
+promise.catch(errore_precedente => mia_callback(errore_precedente))
+
+*/
+
+
 let promise1 = new Promise(function (resolve, reject) {
     // Il corpo di questa funzione si chiama Esecutore, che parte subito e termina chiamando la resolve o la reject.
     // Quando l’esecuzione termina, viene invocato resolve in caso di successo, oppure reject in caso di errore.
@@ -12,6 +30,7 @@ let promise3 = new Promise(function (resolve, reject) {
     // setTimeout(() => resolve('...DONE!'), 3000);
     setTimeout(() => reject(new Error("...ERROR!")), 3000);
 });
+
 promise3
     .then(result => alert(result))
     .catch(error => alert(error))
